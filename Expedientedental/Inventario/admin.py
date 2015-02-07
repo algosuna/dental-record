a4473b312980e24e98bed56634ee2eb31738364a
Raymundo Aviles <raviles@mediexcel.com>
2015-02-07 15:35:26 -0800

    terminado hasta inventario desde equipo ray

--- Expedientedental/Inventario/admin.py
+++ Expedientedental/Inventario/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from Inventario.models import InventarioInsumos
-from Inventario.models import PrecioInsumo
-from Inventario.models import Paquetes
-from Inventario.models import SalidadeMaterial
+from Inventario.models import InsumospPaquete
+from Inventario.models import TipoPaquete
+from Inventario.models import InsumosSalida
+from Inventario.models import SalidadeMaterial 
+
 
 admin.site.register(InventarioInsumos)
-admin.site.register(PrecioInsumo)
-admin.site.register(Paquetes)
+admin.site.register(InsumospPaquete)
+admin.site.register(TipoPaquete)
+admin.site.register(InsumosSalida)
 admin.site.register(SalidadeMaterial)
--- Expedientedental/Inventario/models.py
+++ Expedientedental/Inventario/models.py
@@ -5,35 +5,27 @@ from decimal import Decimal
 # Create your models here.
 
 class InventarioInsumos(models.Model):
-	codigoProveedor=models.CharField(max_length=70)
-	descripcion =models.CharField(max_length=100)
+	tipo_insumo=models.CharField(max_length=50)
+	nombre_producto =models.CharField(max_length=100)
 	cantidad    =models.IntegerField(max_length=6)
-	precioUnidad=models.DecimalField(max_digits=12, decimal_places=2, blank=True, default=0)
-	fechaEntrada= models.DateTimeField(blank=True, null=True)
-	precioCaja = models.DecimalField(max_digits=12, decimal_places=2, blank=True, default=0)
+	precio_unidad=models.DecimalField(max_digits=12, decimal_places=2, blank=True, default=0)
+	fecha_entrada= models.DateTimeField(blank=True, null=True)
 
 	def __unicode__(self):
 		datos="%s %s"%(self.codigoproveedor,self.descripcion)
 		return datos
 
-
-	
-	
-
-class Paquetes(models.Model):
+class TipoPaquete(models.Model):
 	nombrePaquete=models.CharField(max_length=50)
-	codigoProveedor=models.ForeignKey(InventarioInsumos, null =True , related_name='codigoprovee')
-	descripcion = models.ForeignKey(InventarioInsumos, null=True, related_name='descripcion')
-	precioUnidad=codigoproveedor=models.ForeignKey(InventarioInsumos, null =True , related_name='precioUnidad')
-	unidades=models.DecimalField(max_digits=12, decimal_places=2, blank=True, default=0)
 
-	def __unicode__(self):
-		nombrePaq="%s %s"%(self.nombrePaquete)
-		return nombrePaq
 
-		
+class InsumospPaquete(models.Model):
+	inventario_insumos=models.ForeignKey(InventarioInsumos)
+	paquete=models.ForeignKey(TipoPaquete)
+	cantidad=models.IntegerField(max_length=2)
+
 class SalidadeMaterial(models.Model):
-	ordendeSalida=models.IntegerField(unique=True ,max_length=8)
+	insumos_Salida=models.ForeignKey(InventarioInsumos)
 	paciente	= models.ForeignKey(Paciente)
 	medico	= models.ForeignKey(Medico)
 	fechaSalida= models.DateTimeField(blank=True, null=True)
@@ -41,5 +33,15 @@ class SalidadeMaterial(models.Model):
 	def __unicode__(self):
 		codeSalida="%s %s"%(self.ordendeSalida)
 		return codeSalida
+
+class InsumosSalida(models.Model):
+	inventario_insumos=models.ForeignKey(InventarioInsumos)
+	paquete_usado=models.ForeignKey(SalidadeMaterial)
+	cantidad=models.IntegerField(max_length=2)
+
+
+
+		
+
 
 		
