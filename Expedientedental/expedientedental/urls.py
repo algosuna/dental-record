17bfec642523457885df05d846a6b8d4d2340040
Raymundo Aviles <raviles@mediexcel.com>
2015-02-10 07:45:10 -0800

    ultima version con un error en el urls de altas

--- Expedientedental/expedientedental/urls.py
+++ Expedientedental/expedientedental/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 from ActividadesClinicas.views import interrogatorio
 from ActividadesClinicas.views import odontograma
 #from ActividadesClinicas.views import interrogatorio
