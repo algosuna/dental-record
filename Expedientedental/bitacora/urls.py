7aa7d6bb489a829ff6d514f8b6c04fe17027a963
andyosuna <andyosuna@gmail.com>
2015-03-31 07:19:47 -0700

    removed unnecesary apps

--- Expedientedental/bitacora/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns,url
-
-
-urlpatterns = patterns('bitacora.views',
-	
-	url(r'^notas/$','notas_view',name= "notas"),
-	url(r'^bitacoras/$','bitacoras_view',name= "bitacoras"),
-)
-
