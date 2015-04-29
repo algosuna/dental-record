df307506422b5af34af01f619fcfeb75071b7402
andyosuna <agonzalez@mediexcel.com>
2015-04-29 14:59:51 -0700

    changes to cotizacion

--- Expedientedental/cotizacion/views.py
+++ Expedientedental/cotizacion/views.py
@@ -1,19 +1,28 @@
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+
 from wkhtmltopdf.views import PDFTemplateView
+from core.views import LoginRequiredMixin
 
 from cotizacion.models import Cotizacion, CotizacionItem
 from cotizacion.forms import ItemFormSet
 from clinica.models import Odontograma
 
 
-class CotizacionList(ListView):
+class CotizacionList(LoginRequiredMixin, ListView):
     model = Odontograma
     context_object_name = 'orders'
     template_name = 'cotizaciones.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(CotizacionList, self).get_context_data(**kwargs)
+        context.update({'c_active': 'active', })
+        return context
 
+
+@login_required
 def cotizacion_detail(request, odontograma_id):
     odontograma = get_object_or_404(Odontograma, pk=odontograma_id)
     try:
@@ -35,14 +44,17 @@ def cotizacion_detail(request, odontograma_id):
         formset = ItemFormSet(queryset=items)
 
     total = cotizacion.total_aceptado()
+    paciente = cotizacion.odontograma.paciente
+    c_active = 'active'
 
     return render(request, 'cotizacion.html', {
                   'cotizacion': cotizacion,
+                  'paciente': paciente,
                   'items': items,
                   'formset': formset,
-                  'total': total
+                  'total': total,
+                  'c_active': c_active
                   })
-
 
 
 class CotizacionPDF(PDFTemplateView):
