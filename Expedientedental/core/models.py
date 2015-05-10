ecb20205d8a83e2fc0e48cbdc8cca9b8f0a92f41
andyosuna <andyosuna@gmail.com>
2015-05-09 17:08:06 -0700

    added cancelation model and getobjectfromcontext to core

--- Expedientedental/core/models.py
+++ Expedientedental/core/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 
 class TimeStampedModel(models.Model):
-    """Adds a timestamp to the created date and the modified date of the model.
-    Abstract model to add wherever."""
+    ''' Adds a timestamp to the created date and the modified date of the model.
+    Abstract model to add wherever. '''
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class CancelledModel(models.Model):
+    ''' Should be used when a model has the option of getting cancelled. '''
+    created_at = models.DateTimeField(auto_now_add=True)
+    reason = models.TextField()
 
     class Meta:
         abstract = True
