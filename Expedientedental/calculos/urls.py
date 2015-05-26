80cb466ba6f1e6d9a2a96769e3c376d1e4f44a29
Ray <raviles@mediexcel.com>
2015-05-26 08:17:02 -0700

    app , calculo deleted

--- Expedientedental/calculos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from calculos.views import dolarCreate
-
-urlpatterns = patterns(
-    'calculos.views',
-
-    url(r'^divisas/$', dolarCreate.as_view()),
-    )
