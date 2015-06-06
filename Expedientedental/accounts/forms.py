0750f1645a75c7f38aaf26b713101d3180376117
Ray <raviles@mediexcel.com>
2015-06-06 13:02:49 -0700

    accounts:change password done , necesary views in consumidos at deactivated package

--- Expedientedental/accounts/forms.py
+++ Expedientedental/accounts/forms.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 # from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
@@ -26,4 +26,31 @@ class LoginForm(AuthenticationForm):
         self.helper.add_input(
             Submit('submit', 'Acceder', css_class='btn btn-block btn-primary')
         )
-        self.error_messages = self.error_messages
+
+
+class PassChangeForm(PasswordChangeForm):
+    '''
+    Forma para el cambio de contraseña
+    '''
+    error_messages = {
+        'password_incorrect': ('La contraseña propocionada es incorrecta.'),
+        'password_mismatch': ('Las contraseñas no coinciden.')
+    }
+
+    def __init__(self, user,  *args, **kwargs):
+        self.user = user
+        super(PassChangeForm, self).__init__(user, *args, **kwargs)
+        self.helper = FormHelper()
+        self.fields['old_password'].label = 'Contraseña Anterior'
+        self.fields['new_password1'].label = 'Nueva Contraseña'
+        self.fields['new_password2'].label = 'Confirmar Contraseña'
+        self.helper.add_input(
+            Submit('submit', 'Cambiar contraseña',
+                   css_class='btn btn-block btn-primary')
+        )
+
+    def save(self, commit=True):
+        self.user.set_password(self.cleaned_data['new_password1'])
+        if commit:
+            self.user.save()
+        return self.user
